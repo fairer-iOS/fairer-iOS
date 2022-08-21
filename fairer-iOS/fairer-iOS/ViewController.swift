f7aee3759dbfa082ccf53d7c096e6cc2d462b005
Guel-git <eccyuna@gmail.com>
2022-08-21 17:04:42 +0900

    [feat] Start project

---
+++ fairer-iOS/fairer-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2022/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
