9c2e43f4e9172111a2b074246ca21d3f4089dda6
Guel-git <eccyuna@gmail.com>
2022-09-15 17:03:59 +0900

    [ADD] MainButton 기본 구조 생성(#7)

--- fairer-iOS/fairer-iOS/Global/UIComponent/MainButton.swift
+++ fairer-iOS/fairer-iOS/Global/UIComponent/MainButton.swift
@@ -5,4 +5,61 @@
 //  Created by Mingwan Choi on 2022/09/10.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class MainButton: UIButton {
+    private enum Size {
+        static let spacing: CGFloat = 24.0
+        static let height: CGFloat = 56.0
+        static let width: CGFloat = UIScreen.main.bounds.size.width - Size.spacing * 2
+    }
+    
+    // MARK: - property
+    
+    var title: String? {
+        didSet { setupAttribute() }
+    }
+    
+    var isDisabled: Bool = false {
+        didSet { setupAttribute() }
+    }
+        
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+        configUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        render()
+        configUI()
+    }
+    
+    private func render() {
+        self.snp.makeConstraints{
+            $0.width.equalTo(Size.width)
+            $0.height.equalTo(Size.height)
+        }
+    }
+    
+    private func configUI() {
+        layer.masksToBounds = true
+        layer.cornerRadius = 8
+        titleLabel?.font = .title1
+        setTitleColor(.white, for: .normal)
+        setTitleColor(.white, for: .disabled)
+        setTitleColor(.gray800, for: .reserved)
+    }
+    
+    private func setupAttribute() {
+        if let title = title {
+            setTitle(title, for: .normal)
+        }
+        
+        isEnabled = !isDisabled
+    }
+}
