ed7ac6ab24d89a1dfdf1eb1a65b1c812728ccf71
Guel-git <eccyuna@gmail.com>
2022-09-23 01:08:03 +0900

    [ADD] label & label extension 추가(#24)

--- fairer-iOS/fairer-iOS/Screens/Login/LoginViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/Login/LoginViewController.swift
@@ -11,4 +11,47 @@ import SnapKit
 
 final class LoginViewController: BaseViewController {
     
+    // MARK: - property
+    
+    private let logoImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiterals.imgLogoLogin
+        return imageView
+    }()
+    private let loginLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .h3
+        label.textColor = .white
+        label.setTextWithLineHeight(text: "평화로운 집안일,\n페어러가 도와드릴게요", lineHeight: 28)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    // MARK: - lifecycle
+    
+    override func configUI() {
+        view.backgroundColor = .blue
+    }
+    
+    override func render() {
+        view.addSubview(logoImage)
+        logoImage.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(239)
+        }
+        
+        view.addSubview(loginLabel)
+        loginLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(logoImage.snp.bottom).offset(30)
+        }
+    }
+    
+    // MARK: - helper func
+    
+    override func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .blue
+    }
 }
