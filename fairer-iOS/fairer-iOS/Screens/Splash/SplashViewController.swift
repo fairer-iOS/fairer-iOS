f7285dbf5c8aede0cb6a2fc962b46569cd983ebe
Guel-git <eccyuna@gmail.com>
2022-09-22 21:02:43 +0900

    [FEAT] SplashView UI(#22)

--- fairer-iOS/fairer-iOS/Screens/Splash/SplashViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/Splash/SplashViewController.swift
@@ -10,5 +10,23 @@ import UIKit
 import SnapKit
 
 final class SplashViewController: BaseViewController {
+    
+    // MARK: - property
 
+    private let splashLogo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiterals.imgLogoSplash
+        return imageView
+    }()
+    
+    // MARK: - lifecycle
+    
+    override func render() {
+        view.addSubview(splashLogo)
+        splashLogo.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
 }
