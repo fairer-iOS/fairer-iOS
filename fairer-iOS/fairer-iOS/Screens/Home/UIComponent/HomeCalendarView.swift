cd0bb04b8b4bd08b027c79c5bc46f98b166c608d
chemi <wbsldj7645@naver.com>
2022-09-23 16:30:17 +0900

    [ADD] 오늘 버튼 추가 (#21)

--- fairer-iOS/fairer-iOS/Screens/Home/UIComponent/HomeCalendarView.swift
+++ fairer-iOS/fairer-iOS/Screens/Home/UIComponent/HomeCalendarView.swift
@@ -21,6 +21,16 @@ final class HomeCalendarView: BaseUIView {
         picker.subviews.first?.subviews.first?.subviews.first?.backgroundColor = .clear
         return picker
     }()
+    private let todayButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(TextLiteral.homeCalendarViewTodayTitle, for: .normal)
+        button.setTitleColor(.positive20, for: .normal)
+        button.titleLabel?.font = .caption1
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 10
+        button.layer.borderColor = UIColor.positive20.cgColor
+        return button
+    }()
     
     // MARK: - life cycle
     
@@ -34,6 +44,15 @@ final class HomeCalendarView: BaseUIView {
             $0.leading.equalToSuperview().inset(24)
             $0.top.equalToSuperview().inset(8)
             $0.width.equalTo(100)
+        }
+        
+        self.addSubview(todayButton)
+        todayButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(24)
+            $0.height.equalTo(20)
+            // FIXME: - 좋은 방법은 아닌 것 같음
+            $0.width.equalTo(37)
+            $0.centerY.equalTo(calendarPicker.snp.centerY)
         }
     }
 }
