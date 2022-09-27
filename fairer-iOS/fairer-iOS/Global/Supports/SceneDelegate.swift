6cea52d47e51362bc3dc997a85694b7965fd6974
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2022-09-27 20:40:02 +0900

    [CHORE] 신 델리게이트 수정 (#17)

--- fairer-iOS/fairer-iOS/Global/Supports/SceneDelegate.swift
+++ fairer-iOS/fairer-iOS/Global/Supports/SceneDelegate.swift
@@ -15,7 +15,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        //FIXME
         let rootViewControoler = UINavigationController(rootViewController: HomeViewController())
         window?.rootViewController = rootViewControoler
         window?.makeKeyAndVisible()
