01e7ed55599744ba2725a60f53df8d7179545ee7
Guel-git <eccyuna@gmail.com>
2022-09-29 16:25:32 +0900

    [ADD] houseNameTextField & 입력 완료 버튼 추가(#32)

--- fairer-iOS/fairer-iOS/Screens/HouseMakeName/HouseMakeNameViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/HouseMakeName/HouseMakeNameViewController.swift
@@ -35,8 +35,46 @@ final class HouseMakeNameViewController: BaseViewController {
         label.numberOfLines = 0
         return label
     }()
+    private let houseNameTextField: UITextField = {
+        let textField = UITextField()
+        let attributes = [
+            NSAttributedString.Key.font: UIFont.h3,
+            NSAttributedString.Key.foregroundColor: UIColor.gray400
+        ]
+        textField.backgroundColor = .normal0
+        textField.font = .h3
+        textField.attributedPlaceholder = NSAttributedString(string: "예) 즐거운 우리집", attributes: attributes)
+        textField.layer.cornerRadius = 8
+        textField.autocorrectionType = .no
+        textField.autocapitalizationType = .none
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 24, height: 58))
+        textField.leftViewMode = .always
+        textField.setClearButton()
+        return textField
+    }()
+    private let disableLabel: UILabel = {
+        let label = UILabel()
+        label.text = TextLiteral.onboardingNameViewControllerDisableLabel
+        label.textColor = .negative20
+        label.font = .body2
+        label.layer.isHidden = true
+        return label
+    }()
+    private lazy var houseNameDoneButton: MainButton = {
+        let button = MainButton()
+        button.title = "입력 완료"
+        button.isDisabled = true
+        button.addTarget(self, action: #selector(didTapDoneButton), for: .touchUpInside)
+        return button
+    }()
     
     // MARK: - life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupDelegation()
+        setupNotificationCenter()
+    }
 
     override func render() {
         view.addSubview(writeNamePrimaryLabel)
@@ -50,6 +88,25 @@ final class HouseMakeNameViewController: BaseViewController {
             $0.top.equalTo(writeNamePrimaryLabel.snp.bottom)
             $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
         }
+        
+        view.addSubview(houseNameTextField)
+        houseNameTextField.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+            $0.top.equalTo(writeNameSecondaryLabel.snp.bottom).offset(16)
+            $0.height.equalTo(58)
+        }
+        
+        view.addSubview(disableLabel)
+        disableLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+            $0.top.equalTo(houseNameTextField.snp.bottom).offset(8)
+        }
+        
+        view.addSubview(houseNameDoneButton)
+        houseNameDoneButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
+        }
     }
     
     // MARK: - functions
@@ -62,5 +119,68 @@ final class HouseMakeNameViewController: BaseViewController {
         navigationController?.navigationBar.prefersLargeTitles = false
         navigationItem.largeTitleDisplayMode = .never
         navigationItem.leftBarButtonItem = backButton
+    }
+    
+    private func setupDelegation() {
+        houseNameTextField.delegate = self
+    }
+    
+    @objc private func didTapDoneButton() {
+        if houseNameTextField.text!.hasCharacters() {
+            houseNameTextField.layer.borderWidth = 0
+            disableLabel.isHidden = true
+            
+            // TODO: - userdefault에 이름 저장
+            
+        } else {
+            houseNameTextField.layer.borderWidth = 1
+            houseNameTextField.layer.borderColor = UIColor.negative20.cgColor
+            houseNameDoneButton.isDisabled = true
+            disableLabel.isHidden = false
+        }
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.houseNameDoneButton.transform = CGAffineTransform(translationX: 0, y: -keyboardSize.height + 20)
+            })
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification:NSNotification) {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.houseNameDoneButton.transform = .identity
+        })
+    }
+    
+    private func setupNotificationCenter() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+}
+
+// MARK: - extension
+
+extension HouseMakeNameViewController : UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let char = string.cString(using: String.Encoding.utf8) {
+            let isBackSpace = strcmp(char, "\\b")
+            if isBackSpace == -92 {
+                return true
+            }
+        }
+        guard textField.text!.count < 10 else { return false }
+        return true
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        let hasText = houseNameTextField.hasText
+        houseNameDoneButton.isDisabled = !hasText
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        houseNameTextField.layer.borderWidth = 0
+        view.endEditing(true)
     }
 }
