d16292d7a984259a7dc5aea129ec35029bec3088
Guel-git <eccyuna@gmail.com>
2022-09-29 15:54:24 +0900

    [ADD] HouseMakeNameViewController 생성(#32)

---
+++ fairer-iOS/fairer-iOS/Screens/HouseMakeName/HouseMakeNameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HouseMakeNameViewController.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2022/09/29.
+//
+
+import UIKit
+
+class HouseMakeNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
