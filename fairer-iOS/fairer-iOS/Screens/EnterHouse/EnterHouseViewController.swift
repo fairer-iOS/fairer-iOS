322ec2a727df70ab6a53e8725d9309ee727cdf15
Guel-git <eccyuna@gmail.com>
2022-10-15 17:06:38 +0900

    [ADD] textfield delegate, 예외케이스 toast popup 추가(#36)

--- fairer-iOS/fairer-iOS/Screens/EnterHouse/EnterHouseViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/EnterHouse/EnterHouseViewController.swift
@@ -33,14 +33,24 @@ final class EnterHouseViewController: BaseViewController {
         textfield.myPlaceholder = "12글자 입력"
         return textfield
     }()
-    private let enterHouseDoneButton: MainButton = {
+    private lazy var enterHouseDoneButton: MainButton = {
         let button = MainButton()
         button.isDisabled = true
         button.title = TextLiteral.doneButtonText
+        let buttonAction = UIAction { [weak self] _ in
+            self?.touchUpToShowToast()
+        }
+        button.addAction(buttonAction, for: .touchUpInside)
         return button
     }()
 
     // MARK: - lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupDelegation()
+        setupNotificationCenter()
+    }
     
     override func render() {
         view.addSubview(enterHousePrimaryLabel)
@@ -79,4 +89,85 @@ final class EnterHouseViewController: BaseViewController {
         navigationItem.largeTitleDisplayMode = .never
         navigationItem.leftBarButtonItem = backButton
     }
+    
+    private func setupDelegation() {
+        enterHouseCodeTextfield.delegate = self
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.enterHouseDoneButton.transform = CGAffineTransform(translationX: 0, y: -keyboardSize.height + 20)
+            })
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification:NSNotification) {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.enterHouseDoneButton.transform = .identity
+        })
+    }
+    
+    private func setupNotificationCenter() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func touchUpToShowToast() {
+        //FIXME: - 예외 케이스에 따라 분기처리
+        showToast("잘못된 코드입니다.", 36)
+        showToast("하우스가 꽉 차서 참여할 수 없어요.\n6명까지만 참여할 수 있어요.", 56)
+    }
+    
+    private func showToast(_ message: String, _ height: Int) {
+        let toastLabel = UILabel()
+        toastLabel.text = message
+        toastLabel.textColor = .white
+        toastLabel.font = .title2
+        toastLabel.numberOfLines = 0
+        toastLabel.backgroundColor = .gray700
+        toastLabel.textAlignment = .center
+        toastLabel.layer.cornerRadius = 8
+        toastLabel.clipsToBounds = true
+        toastLabel.alpha = 0
+        view.addSubview(toastLabel)
+        toastLabel.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(2)
+            $0.leading.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+            $0.height.equalTo(height)
+        }
+        UIView.animate(withDuration: 0.8, animations: {
+            toastLabel.alpha = 1.0
+        }, completion: { isCompleted in
+            UIView.animate(withDuration: 1.2, animations: {
+                toastLabel.alpha = 0
+            }, completion: { isCompleted in
+                toastLabel.removeFromSuperview()
+            })
+        })
+        
+    }
 }
+
+extension EnterHouseViewController : UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let char = string.cString(using: String.Encoding.utf8) {
+            let isBackSpace = strcmp(char, "\\b")
+            if isBackSpace == -92 {
+                return true
+            }
+        }
+        guard textField.text!.count < 12 else { return false }
+        return true
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        let hasText = enterHouseCodeTextfield.hasText
+        enterHouseDoneButton.isDisabled = !hasText
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+}
+
