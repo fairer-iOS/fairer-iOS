d42d38da2d4ef68af7be9d8fa1dede523eee5e09
Guel-git <eccyuna@gmail.com>
2022-10-16 17:34:23 +0900

    [ADD] HouseInfo 파일 생성(#38)

---
+++ fairer-iOS/fairer-iOS/Screens/HouseInfo/HouseInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HouseInfoViewController.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2022/10/16.
+//
+
+import UIKit
+
+import SnapKit
+
+class HouseInfoViewController: BaseViewController {
+
+    // MARK: - property
+    
+    // MARK: - lifecycle
+
+}
