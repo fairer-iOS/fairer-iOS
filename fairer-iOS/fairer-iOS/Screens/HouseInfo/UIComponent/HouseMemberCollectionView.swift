5732160b62c0e316097aeae68ebc57e8ec796b67
Guel-git <eccyuna@gmail.com>
2022-10-31 22:38:17 +0900

    [ADD] HouseMemberCollectionView & Cell 추가(#38)

---
+++ fairer-iOS/fairer-iOS/Screens/HouseInfo/UIComponent/HouseMemberCollectionView.swift
@@ -0,0 +1,14 @@
+//
+//  HouseMemberCollectionView.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2022/10/31.
+//
+
+import UIKit
+
+import SnapKit
+
+final class HouseMemberCollectionView: BaseUIView {
+    
+}
---
+++ fairer-iOS/fairer-iOS/Screens/HouseInfo/UIComponent/HouseMemberCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HouseMemberCollectionViewCell.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2022/10/31.
+//
+
+import UIKit
+
+import SnapKit
+
+final class HouseMemberCollectionViewCell: BaseCollectionViewCell {
+    
+}
