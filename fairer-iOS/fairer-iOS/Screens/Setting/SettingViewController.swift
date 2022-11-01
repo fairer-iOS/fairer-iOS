ea43341c78b61cd142b7dd6593ce04a26fcbba36
Guel-git <eccyuna@gmail.com>
2022-11-01 23:09:10 +0900

    [ADD] 디렉토리 & 파일 생성(#42)

---
+++ fairer-iOS/fairer-iOS/Screens/Setting/SettingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingViewController.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2022/11/01.
+//
+
+import UIKit
+
+import SnapKit
+
+final class SettingViewController: BaseViewController {
+
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    // MARK: - func
+}
