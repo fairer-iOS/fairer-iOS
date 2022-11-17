7427f69fdd6cc20478e4bc755218a1863d33b79e
Guel-git <eccyuna@gmail.com>
2022-11-17 21:26:07 +0900

    [ADD] 파일 생성 & OnboardingProfileVC 상속 받기(#33)

---
+++ fairer-iOS/fairer-iOS/Screens/SettingProfileImage/SettingProfileImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingProfileImageViewController.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2022/11/17.
+//
+
+import UIKit
+
+final class SettingProfileImageViewController: OnboardingProfileViewController {
+
+}
