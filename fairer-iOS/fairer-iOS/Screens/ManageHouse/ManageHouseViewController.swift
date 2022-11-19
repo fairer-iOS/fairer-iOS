e21a9d73b1752f7b0c7c2f5751210d570d449ff5
Guel-git <eccyuna@gmail.com>
2022-11-19 13:40:02 +0900

    [ADD] 파일 디렉토리 생성(#47)

---
+++ fairer-iOS/fairer-iOS/Screens/ManageHouse/ManageHouseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ManageHouseViewController.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2022/11/19.
+//
+
+import UIKit
+
+final class ManageHouseViewController: BaseViewController {
+
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    // MARK: - func
+}
