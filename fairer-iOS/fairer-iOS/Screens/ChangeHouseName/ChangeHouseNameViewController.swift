12b965d3d8559643cf054fb479efc647380e5a9b
Guel-git <eccyuna@gmail.com>
2022-11-30 23:03:09 +0900

    [ADD] nav bar 생성(#49)

--- fairer-iOS/fairer-iOS/Screens/ChangeHouseName/ChangeHouseNameViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/ChangeHouseName/ChangeHouseNameViewController.swift
@@ -11,7 +11,19 @@ final class ChangeHouseNameViewController: BaseViewController {
 
     // MARK: - property
     
+    private let backButton = BackButton(type: .system)
+    
     // MARK: - life cycle
     
     // MARK: - func
+    
+    override func setupNavigationBar() {
+        super.setupNavigationBar()
+        
+        let backButton = makeBarButtonItem(with: backButton)
+        
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.leftBarButtonItem = backButton
+    }
 }
