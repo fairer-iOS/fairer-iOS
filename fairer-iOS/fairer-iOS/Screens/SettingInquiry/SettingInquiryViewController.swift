5716ec2ccf0197f06a70ae48caa9d4c31b1759a0
Guel-git <eccyuna@gmail.com>
2022-12-20 00:43:05 +0900

    [ADD] title label 생성(#53)

--- fairer-iOS/fairer-iOS/Screens/SettingInquiry/SettingInquiryViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/SettingInquiry/SettingInquiryViewController.swift
@@ -14,8 +14,24 @@ final class SettingInquiryViewController: BaseViewController {
     // MARK: - property
     
     private let backButton = BackButton(type: .system)
+    private let settingInquiryTitleLabel: UILabel = {
+        let label = UILabel()
+        label.setTextWithLineHeight(text: "궁금한 점이 있으시다면\n아래 연락처를 통해 문의해주세요.", lineHeight: 28)
+        label.textColor = .gray800
+        label.font = .h2
+        label.numberOfLines = 2
+        return label
+    }()
     
     // MARK: - life cycle
+    
+    override func render() {
+        view.addSubview(settingInquiryTitleLabel)
+        settingInquiryTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(28)
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+    }
     
     // MARK: - func
     
