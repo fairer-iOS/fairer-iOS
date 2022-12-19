23ac96c40f5e603bf48fc4cce37048b9838a4e81
Guel-git <eccyuna@gmail.com>
2022-12-19 19:20:14 +0900

    [ADD] 파일 & 디렉토리 생성(#51)

---
+++ fairer-iOS/fairer-iOS/Screens/SettingAlarm/SettingAlarmViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingAlarmViewController.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2022/12/19.
+//
+
+import UIKit
+
+final class SettingAlarmViewController: BaseViewController {
+
+}
