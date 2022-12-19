ba15d6db88bd54388c3df823fb8a1c64f06d4aef
Guel-git <eccyuna@gmail.com>
2022-12-20 00:38:31 +0900

    [ADD] nav bar 생성(#53)

--- fairer-iOS/fairer-iOS/Screens/SettingInquiry/SettingInquiryViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/SettingInquiry/SettingInquiryViewController.swift
@@ -13,8 +13,20 @@ final class SettingInquiryViewController: BaseViewController {
 
     // MARK: - property
     
+    private let backButton = BackButton(type: .system)
+    
     // MARK: - life cycle
     
     // MARK: - func
+    
+    override func setupNavigationBar() {
+        super.setupNavigationBar()
+        
+        let backButton = makeBarButtonItem(with: backButton)
+        
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.leftBarButtonItem = backButton
+    }
 
 }
