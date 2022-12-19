797898e56c934218abe2ea105f581e2fc446de7b
Guel-git <eccyuna@gmail.com>
2022-12-20 00:36:47 +0900

    [ADD] 파일 & 디렉토리 추가(#53)

---
+++ fairer-iOS/fairer-iOS/Screens/SettingInquiry/SettingInquiryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingInquiryViewController.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2022/12/20.
+//
+
+import UIKit
+
+import SnapKit
+
+final class SettingInquiryViewController: BaseViewController {
+
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    // MARK: - func
+
+}
