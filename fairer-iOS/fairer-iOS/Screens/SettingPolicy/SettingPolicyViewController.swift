bc67aa90d71fa5022e8c09ed5603d4f4e670f7a9
Guel-git <eccyuna@gmail.com>
2022-12-27 01:29:02 +0900

    [ADD] scroll view 생성(#55)

--- fairer-iOS/fairer-iOS/Screens/SettingPolicy/SettingPolicyViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/SettingPolicy/SettingPolicyViewController.swift
@@ -14,8 +14,17 @@ final class SettingPolicyViewController: BaseViewController {
     // MARK: - property
     
     private let backButton = BackButton(type: .system)
+    private let scrollView = UIScrollView()
     
     // MARK: - life cycle
+    
+    override func render() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
+    }
 
     // MARK: - func
     
