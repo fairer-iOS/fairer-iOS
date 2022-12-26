a7f50218323dfc6b7dc12cb9cf56cf1a659c58f2
Guel-git <eccyuna@gmail.com>
2022-12-27 01:24:36 +0900

    [ADD] 파일 생성(#55)

---
+++ fairer-iOS/fairer-iOS/Screens/SettingPolicy/SettingPolicyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingPolicyViewController.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2022/12/27.
+//
+
+import UIKit
+
+import SnapKit
+
+final class SettingPolicyViewController: BaseViewController {
+
+    // MARK: - property
+    
+    // MARK: - life cycle
+
+}
