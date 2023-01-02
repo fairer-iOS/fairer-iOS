ce999384311c685f36cd5ea7aa684197aa6c641e
Guel-git <eccyuna@gmail.com>
2023-01-02 23:20:03 +0900

    [ADD] nextbutton & scrollview 생성(#57)

--- fairer-iOS/fairer-iOS/Screens/CreateHouseWork/CreateHouseWorkViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/CreateHouseWork/CreateHouseWorkViewController.swift
@@ -14,6 +14,8 @@ final class CreateHouseWorkViewController: BaseViewController {
     // MARK: - property
     
     private let backButton = BackButton(type: .system)
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
     private let createHouseWorkCalendar = CreateHouseWorkCalendarView()
     private let spaceCollectionView = SelectHouseWorkSpaceCollectionView()
     private let spaceInfoLabel: InfoLabelView = {
@@ -34,35 +36,72 @@ final class CreateHouseWorkViewController: BaseViewController {
         button.addAction(action, for: .touchUpInside)
         return button
     }()
+    private let nextButtonBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    private lazy var nextButton: MainButton = {
+        let button = MainButton()
+        button.title = "다음"
+        button.isDisabled = true
+        let action = UIAction { [weak self] _ in
+            self?.didTappedNextButton()
+        }
+        button.addAction(action, for: .touchUpInside)
+        return button
+    }()
     
     // MARK: - life cycle
     
     override func render() {
-        view.addSubview(createHouseWorkCalendar)
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.width.edges.equalToSuperview()
+        }
+        
+        contentView.addSubview(createHouseWorkCalendar)
         createHouseWorkCalendar.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).inset(4)
+            $0.top.equalToSuperview().inset(4)
             $0.leading.equalToSuperview().inset(15)
         }
         
-        view.addSubview(spaceCollectionView)
+        contentView.addSubview(spaceCollectionView)
         spaceCollectionView.snp.makeConstraints {
             $0.top.equalTo(createHouseWorkCalendar.snp.bottom).offset(4)
             $0.horizontalEdges.equalToSuperview()
             $0.height.equalTo(248)
         }
         
-        view.addSubview(spaceInfoLabel)
+        contentView.addSubview(spaceInfoLabel)
         spaceInfoLabel.snp.makeConstraints {
             $0.top.equalTo(spaceCollectionView.snp.bottom)
             $0.horizontalEdges.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
             $0.height.equalTo(22)
         }
         
-        view.addSubview(writeHouseWorkButton)
+        contentView.addSubview(writeHouseWorkButton)
         writeHouseWorkButton.snp.makeConstraints {
             $0.top.equalTo(spaceInfoLabel.snp.bottom).offset(32)
             $0.horizontalEdges.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
             $0.height.equalTo(42)
+        }
+        
+        view.addSubview(nextButtonBackgroundView)
+        nextButtonBackgroundView.snp.makeConstraints {
+            $0.bottom.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(122)
+        }
+        
+        nextButtonBackgroundView.addSubview(nextButton)
+        nextButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(SizeLiteral.componentPadding)
         }
     }
     
@@ -81,5 +120,10 @@ final class CreateHouseWorkViewController: BaseViewController {
     private func didTappedWriteHouseWorkButton() {
         // FIXME: - 집안일 직접 입력하기 페이지로 이동
         print("집안일 직접 입력하기")
+    }
+    
+    private func didTappedNextButton() {
+        // FIXME: - 집안일 설정하기 페이지로 이동
+        print("다음")
     }
 }
