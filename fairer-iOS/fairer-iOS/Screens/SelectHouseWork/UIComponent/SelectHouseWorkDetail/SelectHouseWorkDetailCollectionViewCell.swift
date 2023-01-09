35d70a08b45271e3564f8fcc408ae87a979034d5
Guel-git <eccyuna@gmail.com>
2023-01-10 00:14:57 +0900

    [ADD] SelectHouseWorkDetailCollectionViewCell 구현(#59)

--- fairer-iOS/fairer-iOS/Screens/SelectHouseWork/UIComponent/SelectHouseWorkDetail/SelectHouseWorkDetailCollectionViewCell.swift
+++ fairer-iOS/fairer-iOS/Screens/SelectHouseWork/UIComponent/SelectHouseWorkDetail/SelectHouseWorkDetailCollectionViewCell.swift
@@ -11,5 +11,49 @@ import SnapKit
 
 final class SelectHouseWorkDetailCollectionViewCell: BaseCollectionViewCell {
     
+    override var isSelected: Bool {
+        didSet { setupAttribute() }
+    }
     
+    // MARK: - property
+    
+    private let cellBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.gray100.cgColor
+        view.layer.cornerRadius = 6
+        return view
+    }()
+    private let cellLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .body2
+        label.textAlignment = .center
+        return label
+    }()
+    
+    // MARK: - life cycle
+    
+    override func render() {
+        self.addSubview(cellBackgroundView)
+        cellBackgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        cellBackgroundView.addSubview(cellLabel)
+        cellLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    // MARK: - func
+    
+    private func setupAttribute() {
+        if isSelected {
+            
+        } else {
+            
+        }
+    }
 }
