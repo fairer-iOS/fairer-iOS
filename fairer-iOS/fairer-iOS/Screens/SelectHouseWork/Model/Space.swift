68ab3ccf783202b2a8014d314fda04594fcec891
Guel-git <eccyuna@gmail.com>
2023-01-10 00:01:10 +0900

    [ADD] 집안일 목록 Model 에 추가(59)

--- fairer-iOS/fairer-iOS/Screens/SelectHouseWork/Model/Space.swift
+++ fairer-iOS/fairer-iOS/Screens/SelectHouseWork/Model/Space.swift
@@ -48,5 +48,22 @@ enum Space: String, CaseIterable {
             return ImageLiterals.imgSelectedKitchen
         }
     }
+    
+    var houseWorkList: [String] {
+        switch self {
+        case .entrance:
+            return ["현관 청소", "분리수거하기", "신발 정리"]
+        case .livingRoom:
+            return ["창 청소", "거실 청소", "물건 정리정돈", "환기 시키기", "빨래 돌리기", "빨래 개기", "세탁기 청소"]
+        case .bathroom:
+            return ["화장실 청소", "욕실 용품 정리", "변기 청소", "욕조 청소"]
+        case .outside:
+            return ["쓰레기 버리기", "장보기", "반려동물 산책", "마중 나가기"]
+        case .room:
+            return ["이불 정리", "물건 정리정돈", "화장대 정리", "방 청소", "옷장 정리"]
+        case .kitchen:
+            return ["설거지", "가스렌지 닦기", "냉장고 정리", "부엌 정리정돈", "음식물 쓰레기 버리기", "식사 준비하기", "간식 준비하기"]
+        }
+    }
 }
 
