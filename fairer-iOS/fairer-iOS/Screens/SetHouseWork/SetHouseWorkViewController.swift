f438e54c656907036b04b93b48edd436b7e45e7a
Guel-git <eccyuna@gmail.com>
2023-01-19 15:17:22 +0900

    [ADD] SetHouseWorkViewController 생성(#63)

---
+++ fairer-iOS/fairer-iOS/Screens/SetHouseWork/SetHouseWorkViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SetHouseWorkViewController.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2023/01/19.
+//
+
+import UIKit
+
+final class SetHouseWorkViewController: BaseViewController {
+
+    // MARK: - property
+    
+    // MARK: - render
+    
+    // MARK: - func
+
+}
