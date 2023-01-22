c7b1f1ccb49bf10561652f36354d022306114577
Guel-git <eccyuna@gmail.com>
2023-01-22 12:47:25 +0900

    [ADD] GetManagerView & CollectionView 생성(#65)

---
+++ fairer-iOS/fairer-iOS/Global/UIComponent/GetManagerView/GetManagerCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  GetManagerCollectionView.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2023/01/22.
+//
+
+import UIKit
+
+import SnapKit
+
+final class GetManagerCollectionView: UICollectionView {
+    
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    // MARK: - func
+}
+
+// MARK: - extension
---
+++ fairer-iOS/fairer-iOS/Global/UIComponent/GetManagerView/GetManagerView.swift
@@ -0,0 +1,19 @@
+//
+//  GetManagerView.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2023/01/22.
+//
+
+import UIKit
+
+import SnapKit
+
+final class GetManagerView: BaseUIView {
+    
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    // MARK: - func
+}
