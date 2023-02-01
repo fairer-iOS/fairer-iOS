7932bc731daf7b90637949999711d082b2ac2544
Guel-git <eccyuna@gmail.com>
2023-02-01 19:26:17 +0900

    [ADD] 집안일 이름 라벨 생성(#72);

--- fairer-iOS/fairer-iOS/Screens/WriteHouseWork/WriteHouseWorkViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/WriteHouseWork/WriteHouseWorkViewController.swift
@@ -17,15 +17,28 @@ final class WriteHouseWorkViewController: BaseViewController {
     private let backButton = BackButton(type: .system)
     // FIXME: - PR 머지되면 Calendar View Global 로
     private let writeHouseWorkCalendarView = SetHouseWorkCalendarView()
+    private let houseWorkNameLabel: UILabel = {
+        let label = UILabel()
+        label.setTextWithLineHeight(text: "집안일 이름", lineHeight: 22)
+        label.textColor = .gray600
+        label.font = .title1
+        return label
+    }()
     
     // MARK: - life cycle
     
     override func render() {
-        view.addSubviews(writeHouseWorkCalendarView)
+        view.addSubviews(writeHouseWorkCalendarView, houseWorkNameLabel)
+        
         writeHouseWorkCalendarView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(38)
+        }
+        
+        houseWorkNameLabel.snp.makeConstraints {
+            $0.top.equalTo(writeHouseWorkCalendarView.snp.bottom).offset(6)
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
         }
     }
     
