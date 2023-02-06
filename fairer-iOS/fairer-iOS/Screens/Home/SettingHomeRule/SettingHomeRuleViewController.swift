ab85431c5272f55c78a3cb31ef1bfbe618acc23b
김규철 <rbcjf122@gmail.com>
2023-02-06 17:04:06 +0900

    [ADD] 하우스 규칙 설정 라벨 / 규칙 텍스트 필드 추가(#75)

--- fairer-iOS/fairer-iOS/Screens/Home/SettingHomeRule/SettingHomeRuleViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/Home/SettingHomeRule/SettingHomeRuleViewController.swift
@@ -12,6 +12,45 @@ class SettingHomeRuleViewController: BaseViewController {
     // MARK: - property
     
     private let backButton = BackButton(type: .system)
+    
+    private let settingHomeRulePrimaryLabel: UILabel = {
+        let label = UILabel()
+        label.text = TextLiteral.settingHomeRulePrimaryLabel
+        label.textColor = .black
+        label.font = .h2
+        return label
+    }()
+    private let settingHomeRuleTextFieldLabel: UILabel = {
+        let label = UILabel()
+        label.text = TextLiteral.settingHomeRuleTextFieldLabel
+        label.textColor = .black
+        label.font = .h2
+        return label
+    }()
+    private let settingHomeRuleTextField: TextField = {
+        let textField = TextField()
+        textField.myPlaceholder = TextLiteral.settingHomeRuleTextFieldPlaceholder
+        return textField
+    }()
+    private let settingHomeRuleInfoPin: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiterals.spacePin
+        return imageView
+    }()
+    private let settingHomeRuleInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = TextLiteral.settingHomeRuleInfoLabel
+        label.textColor = .black
+        label.font = .body2
+        return label
+    }()
+    
+    private lazy var settingHomeRuleInfoStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [settingHomeRuleInfoPin,settingHomeRuleInfoLabel])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        return stackView
+    }()
 
     // MARK: - life cycle
      
