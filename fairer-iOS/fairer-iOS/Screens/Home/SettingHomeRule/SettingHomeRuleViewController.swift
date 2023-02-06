6c35723c51b5bf760ed20a6c17aeab603c0fcf21
김규철 <rbcjf122@gmail.com>
2023-02-06 15:44:25 +0900

    [ADD] 파일 생성(#75)

---
+++ fairer-iOS/fairer-iOS/Screens/Home/SettingHomeRule/SettingHomeRuleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingHomeRuleViewController.swift
+//  fairer-iOS
+//
+//  Created by 김규철 on 2023/02/06.
+//
+
+import UIKit
+
+class SettingHomeRuleViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
