8bdd2f675fd4f6708f40a030b79e0461a2fca8e9
Guel-git <eccyuna@gmail.com>
2023-02-16 16:25:14 +0900

    [ADD] PickDateButton 생성(#85)

---
+++ fairer-iOS/fairer-iOS/Global/UIComponent/PickDateButton.swift
@@ -0,0 +1,20 @@
+//
+//  PickDateButton.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2023/02/16.
+//
+
+import UIKit
+
+import SnapKit
+
+final class PickDateButton: UIButton {
+    
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    // MARK: - func
+    
+}
