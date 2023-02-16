d408cd31bbdb5b2c304baac49bf46d0328ba07c3
Guel-git <eccyuna@gmail.com>
2023-02-16 16:33:47 +0900

    [ADD] PickDateButton 구현(#85)

--- fairer-iOS/fairer-iOS/Global/UIComponent/PickDateButton.swift
+++ fairer-iOS/fairer-iOS/Global/UIComponent/PickDateButton.swift
@@ -13,8 +13,40 @@ final class PickDateButton: UIButton {
     
     // MARK: - property
     
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.text = Date().dateToKoreanString
+        label.textColor = .gray800
+        label.font = .title1
+        return label
+    }()
+    private let dateChevron: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiterals.calendarChevron
+        imageView.tintColor = .gray800
+        return imageView
+    }()
+    
     // MARK: - life cycle
     
-    // MARK: - func
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+    }
     
+    required init?(coder: NSCoder) { nil }
+    
+    private func render() {
+        self.addSubviews(dateLabel, dateChevron)
+        
+        dateLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        dateChevron.snp.makeConstraints {
+            $0.leading.equalTo(dateLabel.snp.trailing).offset(4)
+            $0.centerY.equalTo(dateLabel.snp.centerY)
+            $0.width.height.equalTo(20)
+        }
+    }
 }
