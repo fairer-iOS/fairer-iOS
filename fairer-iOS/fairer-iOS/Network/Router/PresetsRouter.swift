69972e0fa59f134d9e101c7143e756f7b744c000
Guel-git <eccyuna@gmail.com>
2023-02-26 16:42:55 +0900

    [ADD] preset router 구현(#98)

--- fairer-iOS/fairer-iOS/Network/Router/PresetsRouter.swift
+++ fairer-iOS/fairer-iOS/Network/Router/PresetsRouter.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+import Moya
+
+enum PresetRouter {
+    case getAllPreset
+}
+
+extension PresetRouter: BaseTargetType {
+    var path: String {
+        switch self {
+        case .getAllPreset:
+            return URLConstant.presets
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getAllPreset:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getAllPreset:
+            return .requestPlain
+        }
+    }
+}
