257f381c5fd43c936371f10b1e94e727a785ba9b
Guel-git <eccyuna@gmail.com>
2023-03-02 16:36:09 +0900

    [ADD] HouseWorksAPI 생성(#102)

--- fairer-iOS/fairer-iOS/Network/API/HouseWorksAPI.swift
+++ fairer-iOS/fairer-iOS/Network/API/HouseWorksAPI.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+import Moya
+
+final class HouseWorksAPI {
+    
+    private var houseWorksProvider = MoyaProvider<HouseWorkRouter>(plugins: [MoyaLoggerPlugin()])
+    
+    init() { }
+    
+    private(set) var postAddHouseWorksData: [HouseWorksResponse]?
+    
+    func postAddHouseWorksAPI(body: [HouseWorksRequest], completion: @escaping ([HouseWorksResponse]?) -> Void) {
+        self.houseWorksProvider.request(.postAddHouseWorks(body: body)) { [self] result in
+            switch result {
+            case .success(let response):
+                do {
+                    self.postAddHouseWorksData = try response.map([HouseWorksResponse]?.self)
+                    completion(postAddHouseWorksData)
+                } catch(let err) {
+                    print(err.localizedDescription)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+}
