af8bdf683f26941902766948e61450fcb0c1de45
Guel-git <eccyuna@gmail.com>
2023-03-02 14:54:01 +0900

    [ADD] TeamsAPI 생성(#101)

--- fairer-iOS/fairer-iOS/Network/API/TeamsAPI.swift
+++ fairer-iOS/fairer-iOS/Network/API/TeamsAPI.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+import Moya
+
+final class TeamsAPI {
+    
+    private var teamsProvider = MoyaProvider<TeamsRouter>(plugins: [MoyaLoggerPlugin()])
+    
+    init() { }
+    
+    private(set) var getTeamInfoData: TeamInfoResponse?
+    
+    func getTeamInfo(completion: @escaping (TeamInfoResponse?) -> Void) {
+        self.teamsProvider.request(.getTeamInfo) { [self] result in
+            switch result {
+            case .success(let response):
+                do {
+                    self.getTeamInfoData = try response.map(TeamInfoResponse?.self)
+                    completion(getTeamInfoData)
+                } catch(let err) {
+                    print(err.localizedDescription)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+}
