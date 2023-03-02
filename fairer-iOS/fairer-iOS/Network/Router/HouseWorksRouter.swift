6165720823c91c668e81c0d5581fd3be5d1ac21b
Guel-git <eccyuna@gmail.com>
2023-03-02 15:17:24 +0900

    [ADD] HouseWorkRouter 생성(#102)

--- fairer-iOS/fairer-iOS/Network/Router/HouseWorksRouter.swift
+++ fairer-iOS/fairer-iOS/Network/Router/HouseWorksRouter.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+import Moya
+
+enum HouseWorkRouter {
+    case postAddHouseWorks
+}
+
+extension HouseWorkRouter: BaseTargetType {
+    var path: String {
+        switch self {
+        case .postAddHouseWorks:
+            return URLConstant.houseWorks
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .postAddHouseWorks:
+            return .post
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .postAddHouseWorks:
+            return .requestJSONEncodable
+        }
+    }
+}
