e60d73b2d9fe79dbfe1152be8f0c21a9094ffbcb
Guel-git <eccyuna@gmail.com>
2023-03-02 16:37:22 +0900

    [ADD] type error 수정(#102)

--- fairer-iOS/fairer-iOS/Network/Router/HouseWorksRouter.swift
+++ fairer-iOS/fairer-iOS/Network/Router/HouseWorksRouter.swift
@@ -10,7 +10,7 @@ import Foundation
 import Moya
 
 enum HouseWorkRouter {
-    case postAddHouseWorks(body: HouseWorksRequest)
+    case postAddHouseWorks(body: [HouseWorksRequest])
 }
 
 extension HouseWorkRouter: BaseTargetType {
