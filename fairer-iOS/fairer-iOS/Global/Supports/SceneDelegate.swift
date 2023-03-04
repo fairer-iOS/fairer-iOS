be1d3af9a4938ca1befed939097b9ae03a27f5ba
Hongjunehuke <junhyeok2166@daum.net>
2023-03-04 16:40:12 +0900

    [CHORE] SceneDelegate

--- fairer-iOS/fairer-iOS/Global/Supports/SceneDelegate.swift
+++ fairer-iOS/fairer-iOS/Global/Supports/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let rootViewController = UINavigationController(rootViewController: HomeViewController())
+        let rootViewController = UINavigationController(rootViewController: LoginViewController())
         window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
     }
