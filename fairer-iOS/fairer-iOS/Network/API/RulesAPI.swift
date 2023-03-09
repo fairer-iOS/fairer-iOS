5c16ebad2e7234bf52ddb2dddeded32f1e03c9e1
김규철 <rbcjf122@gmail.com>
2023-03-09 18:06:11 +0900

    [ADD] Rules API 추가(#109)

--- fairer-iOS/fairer-iOS/Network/API/RulesAPI.swift
+++ fairer-iOS/fairer-iOS/Network/API/RulesAPI.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+import Moya
+
+final class RulesAPI {
+    
+    private var rulesProvider = MoyaProvider<RulesRouter>(plugins: [MoyaLoggerPlugin()])
+    
+    init() {}
+    
+    private enum ResponseData {
+        case getRules
+        case postRules
+        case deleteRules
+    }
+    
+    func getRules(ruleId: String, completion: @escaping (NetworkResult<Any>) -> Void ) {
+        rulesProvider.request(.getRules) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+
