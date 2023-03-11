4d45cfdddc1aa2bbdca10eb925d353a718c0d983
김규철 <rbcjf122@gmail.com>
2023-03-11 15:48:12 +0900

    [ADD] RulesAPI 수정(#109)

--- fairer-iOS/fairer-iOS/Network/API/RulesAPI.swift
+++ fairer-iOS/fairer-iOS/Network/API/RulesAPI.swift
@@ -28,9 +28,49 @@ final class RulesAPI {
                 let statusCode = response.statusCode
                 let data = response.data
                 
+                let networkResult = self.judgeStatus(by: statusCode, data, responseData: .getRules)
+                
             case .failure(let error):
                 print(error)
             }
+        }
+    }
+    
+    private func judgeStatus(by statusCode: Int, _ data: Data, responseData: ResponseData) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        
+        switch statusCode {
+        case 200..<300:
+            switch responseData {
+            case .getRules, .postRules, .deleteRules:
+                return isValidData(data: data, responseData: responseData)
+            }
+        case 400..<500:
+            guard let decodedData = try? decoder.decode(ErrorResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .requestErr(decodedData)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+    
+    private func isValidData(data: Data, responseData: ResponseData) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        switch responseData {
+        case .getRules:
+            guard let decodedData = try? decoder.decode(RulesResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .success(decodedData)
+
+        case .deleteRules, .postRules:
+            guard let decodedData = try? decoder.decode(RulesResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .success(decodedData)
         }
     }
 }
