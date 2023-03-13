5f968863038e5092a808ce7b0f215ce441d34e4c
Guel-git <eccyuna@gmail.com>
2023-03-14 03:00:30 +0900

    [ADD] global thread 로 변경(#101)

--- fairer-iOS/fairer-iOS/Global/Extension/UIImageView+Extension.swift
+++ fairer-iOS/fairer-iOS/Global/Extension/UIImageView+Extension.swift
@@ -9,10 +9,12 @@ import UIKit
 
 extension UIImageView {
     func load(from url: URL) {
-        if let data = try? Data(contentsOf: url) {
-            if let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self.image = image
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.image = image
+                    }
                 }
             }
         }
