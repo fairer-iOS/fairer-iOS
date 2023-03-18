2ba3a674a2dec0be6bf4e25d63a9908382d1d9a5
Guel-git <eccyuna@gmail.com>
2023-03-18 17:35:55 +0900

    [ADD] HouseWorkRequest에 repeatCycle 추가(#102)

--- fairer-iOS/fairer-iOS/Network/DTO/HouseWorks/HouseWorksRequest.swift
+++ fairer-iOS/fairer-iOS/Network/DTO/HouseWorks/HouseWorksRequest.swift
@@ -11,9 +11,16 @@ struct HouseWorksRequest: Codable {
     var assignees: [Int]
     var houseWorkName: String
     // FIXME: - cycle type 4가지로 변경 필요
-    var repeatCycle: String
+    var repeatCycle: RepeatCycleType.RawValue
     var repeatPattern: String?
-    var scheduledDate: String
+    var scheduledDate: String?
     var scheduledTime: String?
     var space: String
+}
+
+enum RepeatCycleType: String {
+    case once = "O"
+    case daily = "D"
+    case week = "W"
+    case month = "M"
 }
