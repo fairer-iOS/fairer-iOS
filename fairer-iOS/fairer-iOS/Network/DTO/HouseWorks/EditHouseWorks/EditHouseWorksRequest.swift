11f6816de232fa0cb3243dfa9f7d39e8d3aa0010
Guel-git <eccyuna@gmail.com>
2023-04-05 16:54:51 +0900

    [ADD] EditHouseWorksDTO 생성(#138)

---
+++ fairer-iOS/fairer-iOS/Network/DTO/HouseWorks/EditHouseWorks/EditHouseWorksRequest.swift
@@ -0,0 +1,8 @@
+//
+//  EditHouseWorksRequest.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2023/04/05.
+//
+
+import Foundation
---
+++ fairer-iOS/fairer-iOS/Network/DTO/HouseWorks/EditHouseWorks/EditHouseWorksResponse.swift
@@ -0,0 +1,8 @@
+//
+//  EditHouseWorksResponse.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2023/04/05.
+//
+
+import Foundation
