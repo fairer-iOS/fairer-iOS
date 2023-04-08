9a0b6d7b3d46cb955594d2d398c9433268f9290e
Guel-git <eccyuna@gmail.com>
2023-04-08 23:09:18 +0900

    [ADD] Response에 옵셔널 처리(#138)

--- fairer-iOS/fairer-iOS/Network/DTO/HouseWorks/EditHouseWorkResponse.swift
+++ fairer-iOS/fairer-iOS/Network/DTO/HouseWorks/EditHouseWorkResponse.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 struct EditHouseWorkResponse: Codable {
-    var houseWorkId: Int
-    var space: String
-    var houseWorkName: String
-    var assignees: [MemberResponse]
-    var scheduledDate: String
-    var scheduledTime: String
-    var success: Bool
-    var repeatCycle: String
-    var repeatPattern: String
-    var repeatEndDate: String
+    var houseWorkId: Int?
+    var space: String?
+    var houseWorkName: String?
+    var assignees: [MemberResponse]?
+    var scheduledDate: String?
+    var scheduledTime: String?
+    var success: Bool?
+    var repeatCycle: String?
+    var repeatPattern: String?
+    var repeatEndDate: String?
 }
