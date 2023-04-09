78601e571d6dd651674286fa4f45052cdf376f36
Guel-git <eccyuna@gmail.com>
2023-04-09 13:48:50 +0900

    [ADD] view & titleLabel 생성(#145)

--- fairer-iOS/fairer-iOS/Screens/HouseWork/WriteHouseWork/View/RepeatAlertView.swift
+++ fairer-iOS/fairer-iOS/Screens/HouseWork/WriteHouseWork/View/RepeatAlertView.swift
@@ -5,13 +5,54 @@
 //  Created by 김유나 on 2023/04/09.
 //
 
-import Foundation
+import UIKit
 
-final class RepeatAlertView: UIView {
+import SnapKit
+
+final class RepeatAlertView: BaseUIView {
     
     // MARK: - property
     
+    private let blurView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.layer.opacity = 0.3
+        return view
+    }()
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    private let titleLabel: UILabel = {
+       let label = UILabel()
+        label.text = "반복 일정 삭제"
+        label.font = .h3
+        label.textColor = .gray800
+        return label
+    }()
+    
     // MARK: - life cycle
+    
+    override func render() {
+        self.addSubview(blurView)
+        blurView.addSubview(backgroundView)
+        backgroundView.addSubviews(titleLabel)
+        
+        blurView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        backgroundView.snp.makeConstraints {
+            $0.width.equalTo(312)
+            $0.height.equalTo(336)
+            $0.center.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().inset(24)
+        }
+    }
     
     // MARK: - func
 }
