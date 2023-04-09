19dccaf018c12850d052ff16380517c794da36b3
김규철 <rbcjf122@gmail.com>
2023-04-09 21:49:58 +0900

    [ADD] SettingInviteCodeViewController 생성(#148)

---
+++ fairer-iOS/fairer-iOS/Screens/Setting/SettingInviteCode/SettingInviteCodeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingInviteCodeViewController.swift
+//  fairer-iOS
+//
+//  Created by 김규철 on 2023/04/09.
+//
+
+import UIKit
+
+class SettingInviteCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
