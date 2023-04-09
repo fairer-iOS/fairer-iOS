31f31eea6d9e8b511b082555542adfd12bedb5ad
김규철 <rbcjf122@gmail.com>
2023-04-09 22:02:29 +0900

    [ADD] SettingInviteCodeView View 구현 및 API 연결(#148)

--- fairer-iOS/fairer-iOS/Screens/Setting/SettingInviteCode/SettingInviteCodeViewController.swift
+++ fairer-iOS/fairer-iOS/Screens/Setting/SettingInviteCode/SettingInviteCodeViewController.swift
@@ -7,23 +7,197 @@
 
 import UIKit
 
-class SettingInviteCodeViewController: UIViewController {
+import SnapKit
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+final class SettingInviteCodeViewController: BaseViewController {
 
-        // Do any additional setup after loading the view.
+    // MARK: - property
+    
+    private let backButton = BackButton()
+    private let houseInvitePrimaryLabel: UILabel = {
+        let label = UILabel()
+        label.setTextWithLineHeight(text: TextLiteral.houseInviteCodeViewControllerPrimaryLabel, lineHeight: 28)
+        label.font = .h2
+        label.textColor = .gray800
+        return label
+    }()
+    private let houseInviteSecondaryLabel: UILabel = {
+        let label = UILabel()
+        label.setTextWithLineHeight(text: TextLiteral.houseInviteCodeViewControllerSecondaryLabel, lineHeight: 26)
+        label.numberOfLines = 0
+        label.font = .body1
+        label.textColor = .gray400
+        return label
+    }()
+    private lazy var inviteCodeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .title1
+        label.textColor = .gray600
+        return label
+    }()
+    private lazy var inviteCodeView: InviteCodeView = {
+        let view = InviteCodeView()
+        return view
+    }()
+    private lazy var validTimeLabel: InfoLabelView = {
+        let view = InfoLabelView()
+        view.textColor = .negative20
+        view.imageColor = .negative20
+        return view
+    }()
+    private lazy var codeInfoLabel: InfoLabelView = {
+        let view = InfoLabelView()
+        view.text = TextLiteral.houseInviteCodeViewControllerCodeInfoLabel
+        view.imageColor = .gray200
+        view.textColor = .gray600
+        return view
+    }()
+    private lazy var inviteCodeButtonView: InviteCodeButtonView = {
+        let view = InviteCodeButtonView()
+        view.isHidden = true
+        return view
+    }()
+    private lazy var refreshCodeButton: MainButton = {
+        let button = MainButton()
+        button.title = TextLiteral.houseInviteCodeViewControllerRefreshButtonText
+        button.isHidden = true
+        let action = UIAction { [weak self] _ in
+            self?.touchUpToRefeshButton()
+        }
+        button.addAction(action, for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - lifecycle
+    
+    override func configUI() {
+        super.configUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getInviteCodeViewInfo()
     }
-    */
-
+    
+    override func render() {
+        view.addSubview(houseInvitePrimaryLabel)
+        houseInvitePrimaryLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(28)
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+        
+        view.addSubview(houseInviteSecondaryLabel)
+        houseInviteSecondaryLabel.snp.makeConstraints {
+            $0.top.equalTo(houseInvitePrimaryLabel.snp.bottom)
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+        
+        view.addSubview(inviteCodeLabel)
+        inviteCodeLabel.snp.makeConstraints {
+            $0.top.equalTo(houseInviteSecondaryLabel.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+        
+        view.addSubview(inviteCodeView)
+        inviteCodeView.snp.makeConstraints {
+            $0.top.equalTo(inviteCodeLabel.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+        
+        view.addSubview(validTimeLabel)
+        validTimeLabel.snp.makeConstraints {
+            $0.top.equalTo(inviteCodeView.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+            $0.height.equalTo(22)
+        }
+        
+        view.addSubview(codeInfoLabel)
+        codeInfoLabel.snp.makeConstraints {
+            $0.top.equalTo(validTimeLabel.snp.bottom).offset(8)
+            $0.leading.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+        
+        view.addSubview(inviteCodeButtonView)
+        inviteCodeButtonView.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(2)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(184)
+        }
+        
+        view.addSubview(refreshCodeButton)
+        refreshCodeButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(SizeLiteral.componentPadding)
+            $0.leading.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+        }
+    }
+    
+    // MARK: - functions
+    
+    override func setupNavigationBar() {
+        super.setupNavigationBar()
+        
+        let backButton = makeBarButtonItem(with: backButton)
+        
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.leftBarButtonItem = backButton
+    }
+    
+    private func setupButtonLayer(validTime: Date) {
+        if validTime > Date() {
+            inviteCodeButtonView.isHidden = false
+        } else {
+            refreshCodeButton.isHidden = false
+        }
+    }
+    
+    private func bindViewData(houseName: String, inviteCode: String, inviteCodeTimeString: String) {
+        inviteCodeLabel.setTextWithLineHeight(text: houseName + TextLiteral.houseInviteCodeViewControllerInviteCodeLabel, lineHeight: 22)
+        inviteCodeLabel.applyColor(to: houseName, with: .blue)
+        inviteCodeView.code = inviteCode
+        inviteCodeButtonView.code = inviteCode
+        validTimeLabel.text = inviteCodeTimeString + TextLiteral.houseInviteCodeViewControllerValidTimeLabel
+    }
+    
+     private func touchUpToRefeshButton() {
+         getInviteCodeInfo { [weak self] data in
+             guard let inviteCode = data.inviteCode else { return }
+             guard let inviteCodeTimeString = data.inviteCodeExpirationDateTime?.iso8601ToKoreanString else { return }
+             guard let inviteCodeTimeDate = data.inviteCodeExpirationDateTime?.iso8601ToDay else { return }
+             
+             self?.inviteCodeView.code = inviteCode
+             self?.inviteCodeButtonView.code = inviteCode
+             self?.validTimeLabel.text = inviteCodeTimeString + TextLiteral.houseInviteCodeViewControllerValidTimeLabel
+             self?.setupButtonLayer(validTime: inviteCodeTimeDate)
+         }
+    }
+    
+    private func getInviteCodeViewInfo() {
+        getInviteCodeInfo { [weak self] data in
+            guard let houseName = data.teamName else { return }
+            guard let inviteCode = data.inviteCode else { return }
+            guard let inviteCodeTimeString = data.inviteCodeExpirationDateTime?.iso8601ToKoreanString else { return }
+            guard let inviteCodeTimeDate = data.inviteCodeExpirationDateTime?.iso8601ToDay else { return }
+            
+            self?.bindViewData(houseName: houseName, inviteCode: inviteCode, inviteCodeTimeString: inviteCodeTimeString)
+            self?.setupButtonLayer(validTime: inviteCodeTimeDate)
+        }
+    }
 }
+
+extension SettingInviteCodeViewController {
+    func getInviteCodeInfo(completion: @escaping (InviteCodeInfoResponse) -> Void) {
+        NetworkService.shared.teams.getInviteCodeInfo { result in
+            switch result {
+            case .success(let response):
+                guard let inviteCodeInfoData = response as? InviteCodeInfoResponse else { return }
+                completion(inviteCodeInfoData)
+            case .requestErr(let error):
+                dump(error)
+            default:
+                print("server error")
+            }
+        }
+    }
+}
+
