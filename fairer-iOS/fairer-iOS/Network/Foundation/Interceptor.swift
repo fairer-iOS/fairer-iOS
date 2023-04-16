4be2f68d7d437fd066c434e655f5a1c80c346a05
김규철 <rbcjf122@gmail.com>
2023-04-16 18:08:24 +0900

    [ADD] Interceptor 파일 생성

---
+++ fairer-iOS/fairer-iOS/Network/Foundation/Interceptor.swift
@@ -0,0 +1,14 @@
+//
+//  Interceptor.swift
+//  fairer-iOS
+//
+//  Created by 김규철 on 2023/04/16.
+//
+
+import Foundation
+
+import Alamofire
+
+final class Interceptor: RequestInterceptor {
+    
+}
