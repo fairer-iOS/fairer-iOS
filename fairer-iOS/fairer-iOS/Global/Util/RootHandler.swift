d3d488682b623fc87ed5d5e7891b79e82dbbbc2e
김규철 <rbcjf122@gmail.com>
2023-04-19 16:03:58 +0900

    [ADD] 최상위 뷰 이동하기 위한 RootHandler 구현(#160)

---
+++ fairer-iOS/fairer-iOS/Global/Util/RootHandler.swift
@@ -0,0 +1,21 @@
+//
+//  RootHandler.swift
+//  fairer-iOS
+//
+//  Created by 김규철 on 2023/04/18.
+//
+
+import UIKit
+
+final class RootHandler {
+    static let shared = RootHandler()
+    
+    func change() {
+        let LoginViewController = OnboardingNameViewController()
+        guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else { return }
+        
+        // 로그인 뷰로 이동
+        sceneDelegate.window?.rootViewController = LoginViewController
+        LoginViewController.navigationController?.setViewControllers([LoginViewController], animated: true)
+    }
+}
