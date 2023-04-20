65d22627a9151200004b4f23f718f99b11a8782a
김규철 <rbcjf122@gmail.com>
2023-04-20 16:41:42 +0900

    [CHORE] Interceptor 수정(#160)

--- fairer-iOS/fairer-iOS/Network/Foundation/Interceptor.swift
+++ fairer-iOS/fairer-iOS/Network/Foundation/Interceptor.swift
@@ -17,21 +17,37 @@ final class Interceptor: RequestInterceptor {
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
         var urlRequest = urlRequest
         urlRequest.setValue(UserDefaultHandler.shared.acceesToken, forHTTPHeaderField: "Authorization")
+        
+        print("adator 적용 \(urlRequest.headers)")
         completion(.success(urlRequest))
     }
     
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+        print("retry 진입")
         guard let response = request.task?.response as? HTTPURLResponse else {
             completion(.doNotRetryWithError(error))
             return
         }
+        
         let statusCode = response.statusCode
         guard request.retryCount < limit, statusCode == 401 else {
             completion(.doNotRetry)
             return
         }
         
-        NetworkService.shared.oauth.postSignIn(socialType: UserDefaultHandler.shared.socialType) { result in
+        UserDefaultHandler.shared.acceesToken = UserDefaultHandler.shared.refershToken
+        
+        getToken { isSuccess in
+            if isSuccess {
+                completion(.retry)
+            } else {
+                completion(.doNotRetry)
+            }
+        }
+    }
+    
+    private func getToken(completion: @escaping(Bool) -> Void) {
+        NetworkService.shared.oauth.getToken(socialType: UserDefaultHandler.shared.socialType) { result in
             switch result {
             case .success(let data):
                 guard let token = data as? AuthResponse else { return }
@@ -39,14 +55,14 @@ final class Interceptor: RequestInterceptor {
                 guard let refreshToken = token.refreshToken else { return }
                 UserDefaultHandler.shared.acceesToken = accessToken
                 UserDefaultHandler.shared.refershToken = refreshToken
-                completion(.retry)
+                completion(true)
             case .requestErr(let error):
                 dump(error)
                 UserDefaultHandler.shared.refershToken = ""
                 UserDefaultHandler.shared.acceesToken = ""
                 RootHandler.shared.change()
             default:
-                completion(.doNotRetry)
+                completion(false)
             }
         }
     }
