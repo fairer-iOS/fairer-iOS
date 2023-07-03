9c48ad2ed4d1ec409d42a8ff5588612ca04110fe
Guel-git <eccyuna@gmail.com>
2023-07-03 23:19:32 +0900

    [ADD] AlarmAPI 구현(#189)

--- fairer-iOS/fairer-iOS/Network/API/AlarmAPI.swift
+++ fairer-iOS/fairer-iOS/Network/API/AlarmAPI.swift
@@ -6,3 +6,85 @@
 //
 
 import Foundation
+
+import Moya
+
+final class AlarmAPI {
+    
+    private var alarmProvider = MoyaProvider<AlarmRouter>(session: Moya.Session(interceptor: Interceptor()), plugins: [MoyaLoggerPlugin()])
+    
+    private enum ResponseData {
+        case getAlarmStatus
+        case putAlarmStatus
+    }
+    
+    func getAlarmStatus(completion: @escaping (NetworkResult<Any>) -> Void) {
+        alarmProvider.request(.getAlarmStatus) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                let httpUrlResponse = response.response
+                let networkResult = self.judgeStatus(by: statusCode, data, response: httpUrlResponse, responseData: .getAlarmStatus)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+    
+    func putAlarmStatus(completion: @escaping (NetworkResult<Any>) -> Void) {
+        alarmProvider.request(.putAlarmStatus) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                let httpUrlResponse = response.response
+                let networkResult = self.judgeStatus(by: statusCode, data, response: httpUrlResponse, responseData: .putAlarmStatus)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+    
+    private func judgeStatus(by statusCode: Int, _ data: Data, response: HTTPURLResponse?, responseData: ResponseData) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        
+        switch statusCode {
+        case 200..<300:
+            if let authorization = response?.allHeaderFields["Authorization"] as? String,
+               let token = authorization.split(separator: " ").last {
+                UserDefaultHandler.accessToken = String(token)
+            }
+            switch responseData {
+            case .getAlarmStatus, .putAlarmStatus:
+                return isValidData(data: data, responseData: responseData)
+            }
+        case 400..<500:
+            guard let decodedData = try? decoder.decode(ErrorResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .requestErr(decodedData)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+    
+    private func isValidData(data: Data, responseData: ResponseData) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        
+        switch responseData {
+        case .getAlarmStatus:
+            guard let decodedData = try? decoder.decode(WorkInfoReponse.self, from: data) else {
+                return .pathErr
+            }
+            return .success(decodedData)
+        case .putAlarmStatus:
+            guard let decodedData = try? decoder.decode(HouseWorksResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .success(decodedData)
+        }
+    }
+}
