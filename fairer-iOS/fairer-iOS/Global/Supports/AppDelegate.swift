8c3adbe6123113d1dfdd2134ded68ef155362d42
Guel-git <eccyuna@gmail.com>
2023-07-05 23:28:13 +0900

    [ADD] saveToken api 연결(#189)

--- fairer-iOS/fairer-iOS/Global/Supports/AppDelegate.swift
+++ fairer-iOS/fairer-iOS/Global/Supports/AppDelegate.swift
@@ -13,58 +13,70 @@ import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         KakaoSDK.initSDK(appKey: "")
         Messaging.messaging().delegate = self
-
+        
         UNUserNotificationCenter.current().delegate = self
         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
         UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { granted, _ in
             if granted {
-                print("알림 등록이 완료되었습니다.")
+                print("Permission accepted")
             }
         }
         application.registerForRemoteNotifications()
         
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
     }
-
-    // foreground 상에서 알림이 보이게끔 해준다.
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.banner, .sound, .badge])
     }
 }
 
 extension AppDelegate: MessagingDelegate {
-
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        // 여기서 이제 서버로 다시 fcm 토큰을 보내줘야 한다!
-        // 그러나 서버가 없기 때문에 이렇게 token을 출력하게 한다.
-        // 이 토큰은 뒤에서 Test할때 필요하다!
-        print("FCM Token: \(fcmToken)")
+        if let fcmToken {
+            NetworkService.shared.fcm.saveToken(token: fcmToken) { result in
+                switch result {
+                case .success(let response):
+                    guard let data = response as? FCMTokenResponse else { return }
+                    
+                    if let fcmToken = data.token {
+                        UserDefaultHandler.fcmToken = fcmToken
+                    }
+                case .requestErr(let errorResponse):
+                    dump(errorResponse)
+                    guard let data = errorResponse as? UserErrorResponse else { return }
+                    print(data.errorMessage)
+                default:
+                    print("sign in error")
+                }
+            }
+        }
     }
 }
