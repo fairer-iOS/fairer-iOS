634bc33adbd5fbed3909d697e48789e46364dc7b
Guel-git <eccyuna@gmail.com>
2023-07-05 21:42:41 +0900

    [ADD] firebase 연동하여 푸시알림 구현(#189)

--- fairer-iOS/fairer-iOS/Global/Supports/AppDelegate.swift
+++ fairer-iOS/fairer-iOS/Global/Supports/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import FirebaseCore
+import FirebaseMessaging
 import KakaoSDKCommon
 
 @main
@@ -17,6 +18,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         KakaoSDK.initSDK(appKey: "")
+        Messaging.messaging().delegate = self
+
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { granted, _ in
+            if granted {
+                print("알림 등록이 완료되었습니다.")
+            }
+        }
+        application.registerForRemoteNotifications()
+        
         return true
     }
 
@@ -34,5 +46,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+}
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+
+    // foreground 상에서 알림이 보이게끔 해준다.
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .badge])
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        // 여기서 이제 서버로 다시 fcm 토큰을 보내줘야 한다!
+        // 그러나 서버가 없기 때문에 이렇게 token을 출력하게 한다.
+        // 이 토큰은 뒤에서 Test할때 필요하다!
+        print("FCM Token: \(fcmToken)")
+    }
 }
