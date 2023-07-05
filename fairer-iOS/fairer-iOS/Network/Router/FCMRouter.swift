a3b32248df29464e6620fd768d5a5e358b19434b
김규철 <rbcjf122@gmail.com>
2023-07-05 21:45:04 +0900

    [ADD] saveToken Router 구현(#192)

--- fairer-iOS/fairer-iOS/Network/Router/FCMRouter.swift
+++ fairer-iOS/fairer-iOS/Network/Router/FCMRouter.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+import Moya
+
+enum FCMRouter {
+    case saveToken(token: String)
+}
+
+extension FCMRouter: BaseTargetType {
+    var path: String {
+        switch self {
+        case .saveToken:
+            return URLConstant.fcm + "/token"
+        }
+    }
+        
+        var method: Moya.Method {
+            switch self {
+            case .saveToken:
+                return .post
+            }
+        }
+        
+        var task: Moya.Task {
+            switch self {
+            case .saveToken(let token):
+                return .requestParameters(parameters: [
+                    "token": token
+                ], encoding: JSONEncoding.default)
+            }
+        }
+    }
