f7488a15249d251f5f7c3298ebdb9fe468e65db6
Guel-git <eccyuna@gmail.com>
2023-08-04 00:25:19 +0900

    [ADD] oauth api 추가(#250)

--- fairer-iOS/fairer-iOS/Network/API/OauthAPI.swift
+++ fairer-iOS/fairer-iOS/Network/API/OauthAPI.swift
@@ -13,6 +13,12 @@ final class OauthAPI {
 
     private let authProvider = MoyaProvider<OauthRouter>(plugins: [MoyaLoggerPlugin()])
     
+    private enum ResponseData {
+        case postSignIn
+        case postLogout
+        case postSignout
+    }
+    
     func postSignIn(socialType: String,
                            completion: @escaping (NetworkResult<Any>) -> Void) {
         authProvider.request(.oauthLogin(clientType: TextLiteral.clientType, socialType: socialType)) { result in
@@ -20,7 +26,38 @@ final class OauthAPI {
             case .success(let response):
                 let statusCode = response.statusCode
                 let data = response.data
-                let networkResult = self.judgeStatus(by: statusCode, data)
+                let httpUrlResponse = response.response
+                let networkResult = self.judgeStatus(by: statusCode, data, response: httpUrlResponse, responseData: .postSignIn)
+                completion(networkResult)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func postLogout(authorization: String, completion: @escaping (NetworkResult<Any>) -> Void) {
+        authProvider.request(.oauthLogout(authorization: authorization)) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                let httpUrlResponse = response.response
+                let networkResult = self.judgeStatus(by: statusCode, data, response: httpUrlResponse, responseData: .postLogout)
+                completion(networkResult)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func postSignout(completion: @escaping (NetworkResult<Any>) -> Void) {
+        authProvider.request(.signout) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                let httpUrlResponse = response.response
+                let networkResult = self.judgeStatus(by: statusCode, data, response: httpUrlResponse, responseData: .postSignout)
                 completion(networkResult)
             case .failure(let error):
                 print(error)
@@ -28,14 +65,14 @@ final class OauthAPI {
         }
     }
         
-    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+    private func judgeStatus(by statusCode: Int, _ data: Data, response: HTTPURLResponse?,  responseData: ResponseData) -> NetworkResult<Any> {
         let decoder = JSONDecoder()
         switch statusCode {
         case 200..<300:
-            guard let decodedData = try? decoder.decode(AuthResponse.self, from: data) else {
-                return .pathErr
+            switch responseData {
+            case .postSignIn, .postLogout, .postSignout:
+                return isValidData(data: data, responseData: responseData)
             }
-            return .success(decodedData)
         case 400..<500:
             guard let decodedData = try? decoder.decode(ErrorResponse.self, from: data) else {
                 return .pathErr
@@ -45,6 +82,19 @@ final class OauthAPI {
             return .serverErr
         default:
             return .networkFail
+        }
+    }
+    
+    private func isValidData(data: Data, responseData: ResponseData) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        switch responseData {
+        case .postSignIn:
+            guard let decodedData = try? decoder.decode(AuthResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .success(decodedData)
+        case .postLogout, .postSignout:
+            return .success(())
         }
     }
 }
