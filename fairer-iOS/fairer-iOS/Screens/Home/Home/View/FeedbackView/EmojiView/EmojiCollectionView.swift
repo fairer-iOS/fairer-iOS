66e0cf4766d8e82109f8359484a170bca54d771d
Guel-git <eccyuna@gmail.com>
2023-08-30 19:26:00 +0900

    [ADD] 디렉토리 및 파일 생성(#265)

---
+++ fairer-iOS/fairer-iOS/Screens/Home/Home/View/FeedbackView/EmojiView/EmojiCollectionView.swift
@@ -0,0 +1,22 @@
+//
+//  EmojiCollectionView.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2023/08/30.
+//
+
+import Foundation
+
+import SnapKit
+
+final class EmojiCollectionView: BaseUIView {
+    
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    // MARK: - func
+    
+}
+
+// MARK: - extension
---
+++ fairer-iOS/fairer-iOS/Screens/Home/Home/View/FeedbackView/EmojiView/EmojiCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiCollectionViewCell.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2023/08/30.
+//
+
+import Foundation
+
+import SnapKit
+
+final class EmojiCollectionViewCell: BaseCollectionViewCell {
+    
+    // MARK: - property
+    
+    // MARK: - life cycle
+}
