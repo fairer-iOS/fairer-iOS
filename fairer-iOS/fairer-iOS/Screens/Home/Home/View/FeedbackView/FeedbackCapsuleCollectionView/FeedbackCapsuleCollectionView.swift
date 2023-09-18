4303c9ccc401db0a04ad726e5882084f35f0b375
Guel-git <eccyuna@gmail.com>
2023-09-18 21:26:12 +0900

    [ADD] CollectionView 파일 생성(#272)

---
+++ fairer-iOS/fairer-iOS/Screens/Home/Home/View/FeedbackView/FeedbackCapsuleCollectionView/FeedbackCapsuleCollectionView.swift
@@ -0,0 +1,8 @@
+//
+//  FeedbackCapsuleCollectionView.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2023/09/18.
+//
+
+import Foundation
---
+++ fairer-iOS/fairer-iOS/Screens/Home/Home/View/FeedbackView/FeedbackCapsuleCollectionView/FeedbackCapsuleCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  FeedbackCapsuleCollectionViewCell.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2023/09/18.
+//
+
+import Foundation
