6bec247754f6dd605b9b72a5be9a14b1713e522b
Guel-git <eccyuna@gmail.com>
2023-09-19 13:14:27 +0900

    [ADD] 이모지 캡슐 셀 새엇ㅇ(#272)

---
+++ fairer-iOS/fairer-iOS/Screens/Home/Home/View/FeedbackView/FeedbackCapsuleCollectionView/FeedbackCapsuleCollectionViewEmojiCell.swift
@@ -0,0 +1,51 @@
+//
+//  FeedbackCapsuleCollectionViewEmojiCell.swift
+//  fairer-iOS
+//
+//  Created by 김유나 on 2023/09/19.
+//
+
+import UIKit
+
+final class FeedbackCapsuleCollectionViewEmojiCell: BaseCollectionViewCell {
+    
+    // MARK: - property
+    
+    private let emojiImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiterals.emojiAngry
+        return imageView
+    }()
+    private let emojiCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "3"
+        label.textColor = .gray800
+        label.font = .caption1
+        return label
+    }()
+    
+    // MARK: - life cycle
+    
+    override func configUI() {
+        self.backgroundColor = .gray100
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.gray200.cgColor
+        self.layer.cornerRadius = 16
+    }
+    
+    override func render() {
+        self.addSubviews(emojiImageView, emojiCountLabel)
+        
+        emojiImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(8)
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(24)
+        }
+        
+        emojiCountLabel.snp.makeConstraints {
+            $0.leading.equalTo(emojiImageView.snp.trailing).offset(4)
+            $0.trailing.equalToSuperview().inset(12)
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
